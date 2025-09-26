a9ae6f2e88f3fafa1bdf6ed4484bab6163ed4910
Joadson Matos <joadson_shadowy@hotmail.com>
2025-09-25 21:32:30 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia("123456", "Nokia Tijolão", "111111111111111", 32);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n-----------------------------\n");
+
+        Console.WriteLine("Smartphone iPhone:");
+        Smartphone iphone = new Iphone("654321", "iPhone 15 Pro", "222222222222222", 256);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
